f7e05d78b7fd0c07d7accacaec9a1ea7a91ecccc
Haoyi Gao <gao.haoy@northeastern.edu>
2025-06-10 21:02:57 -0700

    set up prisma ORM with Supabase PostgresSQL database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,7 @@
-import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!</h1>
+    <Button>Hello World</Button>
   )
 }
